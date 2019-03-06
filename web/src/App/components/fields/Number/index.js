5df1ccaa2d11ab08991fe115f25e5476ec41e38e
uner4s <nicolas.p.poblete@gmail.com>
2019-03-06 19:05:48 -0300

    Add number field

---
+++ web/src/App/components/fields/Number/index.js
@@ -0,0 +1,125 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styles from './styles.css'
+import autobind from 'autobind-decorator'
+import isNumber from 'lodash/isNumber'
+
+const numeral = global.numeral
+if (!numeral) {
+  throw new Error('Numeral is required in global variable')
+}
+
+export default class NComponent extends React.Component {
+  static propTypes = {
+    onChange: PropTypes.func,
+    value: PropTypes.any,
+    useHint: PropTypes.bool,
+    label: PropTypes.any,
+    errorMessage: PropTypes.string,
+    disabled: PropTypes.bool,
+    passProps: PropTypes.object,
+    description: PropTypes.node
+  }
+
+  state = {didMount: false}
+
+  static getDerivedStateFromProps(props, state) {
+    if (props.value && !state.didMount) {
+      const label = isNumber(props.value)
+        ? numeral(props.value).format('0,0.[000000000000000000000]')
+        : ''
+      return {label, didMount: true}
+    }
+    if (state.didMount && state.value !== props.value) {
+      if (props.value) {
+        const label = isNumber(props.value)
+          ? numeral(props.value).format('0,0.[000000000000000000000]')
+          : ''
+        return {label, value: props.value}
+      } else {
+        return {label: '', value: props.value}
+      }
+    }
+
+    return null
+  }
+
+  unformatValue(label) {
+    return label === '' ? undefined : numeral._.stringToNumber(label)
+  }
+
+  formatValue(real) {
+    return isNumber(real) ? numeral(real).format('0,0.[000000000000000000000]') : ''
+  }
+
+  @autobind
+  onChange(event) {
+    const label = event.target.value
+    const {onChange} = this.props
+
+    if (!label) {
+      this.setState({label: ''})
+      onChange(null)
+      return
+    }
+    const value = this.unformatValue(label)
+    const formatted = this.formatValue(event.target.value)
+    if (formatted) {
+      this.setState({label: formatted, value})
+    } else {
+      this.setState({label, value})
+    }
+    onChange(value)
+  }
+
+  getValue() {
+    const {label} = this.state
+    const {value} = this.props
+
+    if (label) return label
+    if (!value) return ''
+    return this.formatValue(value)
+  }
+
+  @autobind
+  onBlur(event) {
+    if (!event.target.value) return
+    const real = this.formatValue(event.target.value)
+    this.setState({label: real})
+  }
+
+  @autobind
+  onKeyDown(event) {
+    const {value, onChange} = this.props
+
+    if (event.keyCode === 8) {
+      if (String(value).length <= 1 || value === 0) {
+        this.setState({label: ''})
+        onChange(null)
+      }
+    }
+  }
+
+  render() {
+    const {label, passProps, description, errorMessage} = this.props
+
+    return (
+      <div>
+        <div className="label">{label}</div>
+        <div className={styles.osInputContainer}>
+          <input
+            className={styles.osInputText}
+            ref="input"
+            value={this.getValue()}
+            onChange={this.onChange}
+            onKeyDown={this.onKeyDown}
+            onBlur={this.onBlur}
+            {...passProps}
+          />
+        </div>
+        <div className={styles.description}>{description}</div>
+        <div className={styles.osInputError}>{errorMessage}</div>
+      </div>
+    )
+  }
+}
