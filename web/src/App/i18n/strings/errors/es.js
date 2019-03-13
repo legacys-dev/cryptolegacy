fac3bf81af39a4b49e1e8aeb2181fa43ec057c92
uner4s <nicolas.p.poblete@gmail.com>
2019-03-13 12:39:23 -0300

    Add placeholder for errors in fields

--- web/src/App/i18n/strings/errors/es.js
+++ web/src/App/i18n/strings/errors/es.js
@@ -1,21 +1,30 @@
+const message = (label, placeholder) => {
+  if (label !== 'This field') return label
+  return placeholder
+}
+
 export default {
-  stringTooShort: ({label}) => `${label} no tiene el largo suficiente`,
-  notInSchema: ({label}) => `${label} no esta permitido`,
-  required: ({label}) => `${label} no es opcional`,
-  unknownFieldType: ({label}) => `${label} tiene un tipo desconocido`,
-  notAnArray: ({label}) => `${label} no es un arreglo`,
-  notAnObject: ({label}) => `${label} no es un objeto`,
-  notAString: ({label}) => `${label} no es un texto`,
-  notADate: ({label}) => `${label} no es una fecha valida`,
-  notAnInteger: ({label}) => `${label} no es un nuúmero entero`,
-  notANumber: ({label}) => `${label} no es un número`,
-  notAnId: ({label}) => `${label} no es un ID válido`,
-  stringTooLong: ({label}) => `El largo es mayor al permitido`,
-  numberTooSmall: ({label}) => `${label} es un número muy pequeño`,
-  numberTooBig: ({label}) => `${label} es un número muy grande`,
-  notABoolean: ({label}) => `${label} no es un valor verdadero o falso`,
-  notAnEmail: ({label}) => `${label} no es un email`,
-  notUnique: ({label}) => `${label} no es único`,
-  notFound: ({label}) => `${label} no se encontró`,
+  stringTooShort: ({label, placeholder}) =>
+    `${message(label, placeholder)} no tiene el largo suficiente`,
+  notInSchema: ({label, placeholder}) => `${message(label, placeholder)} no esta permitido`,
+  required: ({label, placeholder}) => `${message(label, placeholder)} no es opcional`,
+  unknownFieldType: ({label, placeholder}) =>
+    `${message(label, placeholder)} tiene un tipo desconocido`,
+  notAnArray: ({label, placeholder}) => `${message(label, placeholder)} no es un arreglo`,
+  notAnObject: ({label, placeholder}) => `${message(label, placeholder)} no es un objeto`,
+  notAString: ({label, placeholder}) => `${message(label, placeholder)} no es un texto`,
+  notADate: ({label, placeholder}) => `${message(label, placeholder)} no es una fecha valida`,
+  notAnInteger: ({label, placeholder}) => `${message(label, placeholder)} no es un nuúmero entero`,
+  notANumber: ({label, placeholder}) => `${message(label, placeholder)} no es un número`,
+  notAnId: ({label, placeholder}) => `${message(label, placeholder)} no es un ID válido`,
+  stringTooLong: `El largo es mayor al permitido`,
+  numberTooSmall: ({label, placeholder}) =>
+    `${message(label, placeholder)} es un número muy pequeño`,
+  numberTooBig: ({label, placeholder}) => `${message(label, placeholder)} es un número muy grande`,
+  notABoolean: ({label, placeholder}) =>
+    `${message(label, placeholder)} no es un valor verdadero o falso`,
+  notAnEmail: ({label, placeholder}) => `${message(label, placeholder)} no es un email`,
+  notUnique: ({label, placeholder}) => `${message(label, placeholder)} no es único`,
+  notFound: ({label, placeholder}) => `${message(label, placeholder)} no se encontró`,
   mustStartWithPlus: 'El teléfono debe empezar con +'
 }
