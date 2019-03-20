2a2dfa18811e72cf2dcf362578fbb531a84f134c
uner4s <nicolas.p.poblete@gmail.com>
2019-03-20 20:21:21 -0300

    add function to get partSize, number of parts left in multi upload function

---
+++ server/app/helpers/awsGlacier/multiUpload/getPartSize.js
@@ -0,0 +1,11 @@
+export default function(fileSize) {
+  const glacierMaxParts = 10000
+  let n = 20
+  let partSize = 1024 * 1024 // 1MB chunks
+
+  while (fileSize / partSize > glacierMaxParts) partSize = Math.pow(2, (n += 1))
+
+  const numPartsLeft = Math.ceil(fileSize / partSize)
+
+  return {partSize, numPartsLeft, fileSize}
+}
