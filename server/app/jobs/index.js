6b55f3f0c5abdeb410c44c416392791e1c7181ca
uner4s <nicolas.p.poblete@gmail.com>
2019-03-22 21:17:02 -0300

    create job for delete s3 files. (glacier uploaded: true)

--- server/app/jobs/index.js
+++ server/app/jobs/index.js
@@ -2,8 +2,10 @@ import {start} from '@orion-js/jobs'
 import smallFilesUploader from './smallFilesUploader'
 import mediumFilesUploader from './mediumFilesUploader'
 import bigFilesUploader from './bigFilesUploader'
+import deleteDataFromS3 from './deleteDataFromS3'
 
 start({
+  deleteDataFromS3,
   bigFilesUploader,
   mediumFilesUploader,
   smallFilesUploader
