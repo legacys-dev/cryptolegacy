5255b6bd7f50b1e4794e19fbef918a813b87d324
uner4s <nicolas.p.poblete@gmail.com>
2019-03-24 22:56:53 -0300

    add glacier download request

---
+++ server/app/collections/DownloadRequests/index.js
@@ -0,0 +1,8 @@
+import {Collection} from '@orion-js/app'
+import DownloadRequest from 'app/models/DownloadRequest'
+
+export default new Collection({
+  name: 'glacier_download_requests',
+  model: DownloadRequest,
+  indexes: []
+})
--- server/app/resolvers/Files/index.js
+++ server/app/resolvers/Files/index.js
@@ -2,10 +2,14 @@ import createS3Upload from './createS3Upload'
 import completeS3Upload from './completeS3Upload'
 import files from './files'
 import file from './file'
+import glacierDownloadRequest from './glacierDownloadRequest'
+import getObject from './getObject'
 
 export default {
+  glacierDownloadRequest,
   file,
   files,
   completeS3Upload,
-  createS3Upload
+  createS3Upload,
+  getObject
 }
