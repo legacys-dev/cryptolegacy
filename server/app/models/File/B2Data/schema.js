86876794ad665a11173f2cc90b11bed0c2d9b426
uner4s <nicolas.p.poblete@gmail.com>
2019-04-02 20:02:44 -0300

    Add backblaze b2 data schema

---
+++ server/app/models/File/B2Data/schema.js
@@ -0,0 +1,14 @@
+export default {
+  fileId: {
+    type: 'ID',
+    private: true
+  },
+  bucketId: {
+    type: String,
+    private: true
+  },
+  contentSha1: {
+    type: String,
+    private: true
+  }
+}
