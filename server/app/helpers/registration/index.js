28bc3cb4a9f85e9e6de25eb62a04d4ed9abba0d1
uner4s <nicolas.p.poblete@gmail.com>
2019-04-11 16:49:47 -0400

    Change registration helpers call. Add password validation

---
+++ server/app/helpers/registration/index.js
@@ -0,0 +1,6 @@
+import emailRegistration from './emailRegistration'
+import emailTest from './emailTest'
+import passwordRegistration from './passwordRegistration'
+import passwordValidator from './passwordValidator'
+
+export {emailRegistration, emailTest, passwordRegistration, passwordValidator}
