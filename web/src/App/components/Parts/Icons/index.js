08cf9446be6c6ad6cf7b6c9a18fb8aa64937dae6
uner4s <nicolas.p.poblete@gmail.com>
2019-04-18 18:13:22 -0400

    Add  new Icons

---
+++ web/src/App/components/Parts/Icons/index.js
@@ -0,0 +1,9 @@
+import Cloud from './Cloud'
+import DataStorage from './DataStorage'
+import Configuration from './Configuration'
+import Download from './Download'
+import Trash from './Trash'
+import Options from './Options'
+import UserIcon from './UserIcon'
+
+export {Cloud, Configuration, DataStorage, Download, Options, Trash, UserIcon}
