107c010f393e9097e2586ca5c2a98c0886cd9727
uner4s <nicolas.p.poblete@gmail.com>
2019-04-21 23:31:45 -0400

    Add delete personal vault resolver

--- server/app/resolvers/PersonalVaults/index.js
+++ server/app/resolvers/PersonalVaults/index.js
@@ -2,10 +2,12 @@ import personalVault from './personalVault'
 import createPersonalVault from './createPersonalVault'
 import updatePersonalVault from './updatePersonalVault'
 import personalVaults from './personalVaults'
+import deletePersonalVault from './deletePersonalVault'
 
 export default {
+  deletePersonalVault,
   personalVaults,
   updatePersonalVault,
   createPersonalVault,
-  personalVault,
+  personalVault
 }
