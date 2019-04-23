b5ce0c29b5371dd28d58c978aa155c8637c86eb7
uner4s <nicolas.p.poblete@gmail.com>
2019-04-22 20:23:34 -0400

    create get size helper

---
+++ web/src/App/helpers/files/getSize.js
@@ -0,0 +1,15 @@
+import numeral from 'numeral'
+
+export default function(size) {
+  if (!size) return '0 Bytes'
+
+  if (size > 0 && size < 1024) return `${numeral(size).format('0,0.[00]')} Bytes`
+
+  if (size >= 1024 && size < 1024 * 1024) return `${numeral(size / 1024).format('0,0.[00]')} KB`
+
+  if (size >= 1024 * 1024 && size < 1024 * 1024 * 1000) { return `${numeral(size / 1024).format('0,0.[00]')} MB` }
+
+  if (size >= 1024 * 1024 * 1000 && size < 1024 * 1024 * 1000 * 1000) { return `${numeral(size / 1024).format('0,0.[00]')} GB` }
+
+  if (size >= 1024 * 1024 * 1000 * 1000) return `${numeral(size / 1024).format('0,0.[00]')} TB`
+}
