53173e05968a97af4ffe4a20fde422c522633510
uner4s <nicolas.p.poblete@gmail.com>
2019-04-22 20:23:23 -0400

    Add translate for vaults

--- web/src/App/i18n/strings/index.js
+++ web/src/App/i18n/strings/index.js
@@ -3,11 +3,13 @@ import errors from './errors'
 import settings from './settings'
 import global from './global'
 import files from './files'
+import vaults from './vaults'
 
 export default {
   global,
   settings,
   errors,
   auth,
-  files
+  files,
+  vaults
 }
