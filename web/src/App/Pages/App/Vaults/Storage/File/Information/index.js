ec03b021c02d9c161e6fa766cd9cdd049494e373
uner4s <nicolas.p.poblete@gmail.com>
2019-04-22 20:28:05 -0400

    add default view detail for archive

---
+++ web/src/App/Pages/App/Vaults/Storage/File/Information/index.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styles from './styles.css'
+import Section from 'App/components/Section'
+import getSize from 'App/helpers/files/getSize'
+import moment from 'moment'
+
+const storage = {
+  b2: 'simple storage',
+  glacier: 'high security storage'
+}
+
+export default class Information extends React.Component {
+  static propTypes = {
+    file: PropTypes.object
+  }
+
+  renderInformation(title, info) {
+    return (
+      <div className={styles.content}>
+        <div className="row">
+          <div className="col-xs-12 col-sm-3 end-sm" style={{color: '#999999'}}>
+            {title}
+          </div>
+          <div className="col-xs-12 col-sm-9 start-sm">{info}</div>
+        </div>
+      </div>
+    )
+  }
+
+  render() {
+    const {file} = this.props
+    if (!file) return <span />
+    return (
+      <div className={styles.container}>
+        <Section title="Archivo" description="description">
+          <div className={styles.file}>
+            {this.renderInformation('Nombre:', file.s3Data.name)}
+            {this.renderInformation('Tipo:', file.s3Data.type)}
+            {this.renderInformation('Peso:', getSize(file.s3Data.size))}
+            {this.renderInformation('Bóveda:', file.vaultName)}
+            {this.renderInformation('Tipo de almacenamiento:', storage[file.storage])}
+            {this.renderInformation('Creado:', moment(file.createdAt).format('LL'))}
+          </div>
+        </Section>
+      </div>
+    )
+  }
+}
--- web/src/App/Pages/App/Vaults/Storage/File/index.js
+++ web/src/App/Pages/App/Vaults/Storage/File/index.js
@@ -1,15 +1,42 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import styles from './styles.css'
+import withGraphQL from 'react-apollo-decorators/lib/withGraphQL'
+import Breadcrumbs from 'App/components/Breadcrumbs'
+import gql from 'graphql-tag'
+import Information from './Information'
 
+@withGraphQL(gql`
+  query file($fileId: ID) {
+    file(fileId: $fileId) {
+      _id
+      userVaultId
+      s3Data {
+        name
+        type
+        size
+        status
+      }
+      storage
+      createdAt
+      vaultName
+    }
+  }
+`)
 export default class File extends React.Component {
   static propTypes = {
-
+    file: PropTypes.object
   }
 
-  render () {
+  render() {
+    const {file} = this.props
+    if (!file) return <span />
     return (
       <div className={styles.container}>
-        File
+        <Breadcrumbs past={{[`/vaults/storage/${file.userVaultId}`]: `Bóveda - ${file.vaultName}`}}>
+          Archivo - {file.s3Data.name}
+        </Breadcrumbs>
+        <Information file={file} />
       </div>
     )
   }
