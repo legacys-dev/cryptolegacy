9e47e8783abe592ebbc497cb3fd666c79906a591
uner4s <nicolas.p.poblete@gmail.com>
2019-04-24 20:01:49 -0400

    create s3 get header

---
+++ server/app/helpers/awsS3/getHeadersElement.js
@@ -0,0 +1,18 @@
+import AWS from 'aws-sdk'
+import {AWSCredentials} from './credentials'
+
+export default async function({key, bucket}) {
+  const {accessKeyId, secretAccessKey, region} = AWSCredentials
+  const s3 = new AWS.S3({accessKeyId, secretAccessKey, region})
+
+  const params = {Bucket: bucket, Key: key}
+
+  const result = await new Promise((resolve, reject) => {
+    s3.headObject(params, function(error, data) {
+      if (error) reject(error)
+      else resolve(data)
+    })
+  })
+
+  return result
+}
