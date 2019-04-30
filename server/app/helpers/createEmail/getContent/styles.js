f9669719f08a2cb9df5dbe57f53f9ebc98cda174
uner4s <nicolas.p.poblete@gmail.com>
2019-04-30 19:17:34 -0400

    Add emails helper

---
+++ server/app/helpers/createEmail/getContent/styles.js
@@ -0,0 +1,126 @@
+export default `<style>
+@import url('http://fonts.googleapis.com/css?family=Titillium+Web');
+body, p, a, h1, h2, h3 {
+  font-family: 'Titillium Web', 'Helvetica Neue', Helvetica, sans-serif;
+}
+p, button, a, body {
+  font-size: 15px;
+  line-height: 1.6em;
+  margin: 0;
+  padding: 0;
+  color: #616161;
+}
+h1,
+h2,
+h3 {
+  color: #6c6c6c;
+  font-weight: 500;
+  line-height: 1.2em;
+  margin: 20px 0;
+}
+h1 {
+  font-size: 36px;
+}
+h2 {
+  font-size: 28px;
+}
+h3 {
+  font-size: 22px;
+}
+p,
+ul,
+ol {
+  font-weight: normal;
+  margin: 5px 0;
+}
+ul li,
+ol li {
+  margin-left: 5px;
+  list-style-position: inside;
+}
+.email {
+  background-image: url('https://images.unsplash.com/photo-1551210414-da18c36186cb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80');
+  background-size: cover;
+  background-position: center;
+}
+img.logo {
+  width: 280px;
+  height: 35px;
+  max-width: 100%;
+  margin-top: 40px;
+  margin-bottom: 20px;
+}
+body {
+
+}
+.header {
+
+  text-align: center;
+}
+.content {
+  padding: 10px;
+}
+.content-inner {
+  padding: 10px;
+  max-width: 600px;
+  background-color: #fff;
+  border-radius: 4px;
+  margin: 0 auto;
+}
+.content a {
+  text-decoration: none;
+  color: #42a5f5;
+}
+.footer {
+  max-width: 500px;
+  margin: 0 auto;
+  text-align: center;
+  padding: 20px;
+}
+.footer p {
+  color: #fff;
+}
+.footer a {
+  font-size: 16px;
+  color: #fff !important;
+}
+.content a.button {
+  font-size: 16px;
+  font-weight: bold;
+  border-radius: 5px;
+  text-decoration: none;
+  color: #fff;
+  background-color: #0f2ff1;
+  height: 40px;
+  line-height: 40px;
+  outline: 0;
+  padding: 0px 14px;
+  vertical-align: middle;
+  text-align: center;
+  display: block;
+  margin: 20px auto 0 auto;
+  max-width: 200px;
+  box-shadow: 0 4px 6px rgba(50,50,93,.11), 0 1px 3px rgba(0,0,0,.08);
+}
+.right {
+  float: right;
+}
+.label-small {
+  background-color: #eee;
+  padding: 2.5px 5px;
+  font-size: 0.8rem;
+  position: relative;
+  top: -1.5px;
+  border-radius: 4px;
+}
+.small-title {
+  font-size: 20px;
+  margin-top: 5px;
+}
+.divider {
+  height: 1px;
+  overflow: hidden;
+  background-color: #e0e0e0;
+}
+</style>
+`
