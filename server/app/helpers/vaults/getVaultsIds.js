fa2d4342bed667da3df7e4fce9cb54adb4ebe249
uner4s <nicolas.p.poblete@gmail.com>
2019-05-28 13:34:15 -0400

    Add vault helper group and get vault ids function

---
+++ server/app/helpers/vaults/getVaultsIds.js
@@ -0,0 +1,12 @@
+import isEmpty from 'lodash/isEmpty'
+
+export default function(vaultsCredentials) {
+  if (isEmpty(vaultsCredentials)) return []
+  const vaultsId = []
+
+  for (const credential of vaultsCredentials) {
+    vaultsId.push(credential.vaultId)
+  }
+
+  return vaultsId
+}
