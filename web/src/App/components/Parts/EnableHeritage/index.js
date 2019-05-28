672510f453bc96d5e62da52443ddcb6acfbe1a29
uner4s <nicolas.p.poblete@gmail.com>
2019-05-28 16:52:44 -0400

    Create enable heritage mutation button

---
+++ web/src/App/components/Parts/EnableHeritage/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import styles from './styles.css'
+import MutationButton from 'App/components/MutationButton'
+
+export default function EnableHeritage(props) {
+  return (
+    <div className={styles.container}>
+      <MutationButton
+        title="Habilitar herencia"
+        message="¿Confirmas que quieres habilitar esta herencia?"
+        confirmText="Habilitar"
+        mutation="releaseHeritage"
+        danger
+        params={{heritageId: props.heritageId}}
+        onSuccess={() => props.onEnableSuccess()}>
+        <div className={styles.enable}>Habilitar</div>
+      </MutationButton>
+    </div>
+  )
+}
