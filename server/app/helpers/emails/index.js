6ecb1e3abf71742ee0a48c1e592aa926217152d7
uner4s <nicolas.p.poblete@gmail.com>
2019-05-28 15:38:11 -0400

    set send email for heritages

--- server/app/helpers/emails/index.js
+++ server/app/helpers/emails/index.js
@@ -1,4 +1,7 @@
 import verifyEmail from './verifyEmail'
 import accountCreated from './accountCreated'
+import heritageCreated from './heritageCreated'
+import heritageAvailable from './heritageAvailable'
+import heritageReclaimed from './heritageReclaimed'
 
-export {verifyEmail, accountCreated}
+export {verifyEmail, accountCreated, heritageCreated, heritageReclaimed, heritageAvailable}
