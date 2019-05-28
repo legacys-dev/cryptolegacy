9d4bb614e401b7404a8e4ad0ea693a70f7886636
uner4s <nicolas.p.poblete@gmail.com>
2019-05-28 13:35:36 -0400

    Update empty trash delete way

---
+++ server/app/jobs/removeFiles/index.js
@@ -0,0 +1,41 @@
+import {job} from '@orion-js/jobs'
+import Files from 'app/collections/Files'
+import {deleteArchive as deleteFileInS3} from 'app/helpers/awsS3'
+import {deleteArchive as deleteFileInB2} from 'app/helpers/backblazeB2'
+import {deleteArchive as deleteFileInGlacier} from 'app/helpers/awsGlacier'
+import isEmpty from 'lodash/isEmpty'
+
+export default job({
+  type: 'recurrent',
+  runEvery: 1000 * 60 * 10,
+  async run(params) {
+    const files = await Files.find({status: 'authorizedToRemove'}).toArray()
+
+    if (isEmpty(files)) return
+
+    for (const file of files) {
+      const {s3Data, b2Data, glacierData} = file
+      let hasError = false
+
+      if (!s3Data.deletedFromS3) {
+        const {key, bucket} = s3Data
+        await deleteFileInS3({key, bucket})
+      }
+
+      if (s3Data.deletedFromS3 && file.storage === 'b2') {
+        const fileName = s3Data.name
+        const {fileId} = b2Data
+        const response = await deleteFileInB2({fileName, fileId})
+        if (!response) hasError = true
+      }
+
+      if (s3Data.deletedFromS3 && file.storage === 'glacier') {
+        const {archiveId, vaultName} = glacierData
+        const response = await deleteFileInGlacier({archiveId, vaultName})
+        if (!response) hasError = true
+      }
+
+      if (!hasError) await file.remove()
+    }
+  }
+})
