c46ccc8ae2aad2a8969defd12394ea3579c2346f
uner4s <nicolas.p.poblete@gmail.com>
2019-06-12 19:09:21 -0400

    fix jest.config

--- server/jest.config.js
+++ server/jest.config.js
@@ -166,7 +166,7 @@ module.exports = {
   // A map from regular expressions to paths to transformers
   transform: {
     '^.+\\.[jt]sx?$': require.resolve('babel-jest')
-  }
+  },
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   // transformIgnorePatterns: [
@@ -177,7 +177,7 @@ module.exports = {
   // unmockedModulePathPatterns: undefined,
 
   // Indicates whether each individual test should be reported during the run
-  // verbose: null,
+  verbose: true
 
   // An array of regexp patterns that are matched against all source file paths before re-running tests in watch mode
   // watchPathIgnorePatterns: [],
