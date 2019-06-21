a759786e09529a9aa29f610b118bdfe7cec62825
uner4s <nicolas.p.poblete@gmail.com>
2019-06-21 13:05:05 -0400

    Create vault policy owner permission

--- server/app/helpers/permissions/vaultPoliciesChecker/index.js
+++ server/app/helpers/permissions/vaultPoliciesChecker/index.js
@@ -1,13 +1,19 @@
 import cloneDeep from 'lodash/cloneDeep'
 import vaultPolicyPermissions from './vaultPolicyPermissions'
+import vaultPolicyOwnerPermission from './vaultPolicyOwnerPermission'
 
 export default async function(options, viewer, {params}) {
   params = cloneDeep(params)
 
-  const {vaultPoliciesPaginatedPermissions} = options
+  const {vaultPoliciesPaginatedPermissions, vaultPolicyOwner} = options
 
   if (vaultPoliciesPaginatedPermissions) {
     const {vaultId, filter, adminPanel, status} = params
     await vaultPolicyPermissions({vaultId, filter, adminPanel, status, viewer})
+  }
+
+  if (vaultPolicyOwner) {
+    const {vaultId} = params
+    await vaultPolicyOwnerPermission({vaultId, viewer})
   }
 }
---
+++ server/app/helpers/permissions/vaultPoliciesChecker/vaultPolicyOwnerPermission.js
@@ -0,0 +1,14 @@
+import {PermissionsError} from '@orion-js/app'
+import VaultPolicies from 'app/collections/VaultPolicies'
+
+export default async function({vaultId, viewer}) {
+  if (!vaultId) throw new Error('Vault identificator required')
+
+  const vaultPolicy = await VaultPolicies.findOne({vaultId})
+
+  if (!vaultPolicy) throw new Error('Vault credentials not found')
+
+  if (vaultPolicy.userId !== viewer.userId) {
+    throw new PermissionsError('unauthorized', {message: 'You dont have vault credentials'})
+  }
+}
