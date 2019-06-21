8848e6b6f0c7302c6c76d9956c70388585815774
uner4s <nicolas.p.poblete@gmail.com>
2019-06-20 23:55:06 -0400

    Delete old functions for heritages and vault credentials

--- server/app/collections/Heritages/index.js
+++
@@ -1,8 +0,0 @@
-import {Collection} from '@orion-js/app'
-import Heritage from 'app/models/Heritage'
-
-export default new Collection({
-  name: 'heritages',
-  model: Heritage,
-  indexes: [{keys: {inheritorEmail: 1, vaultId: 1}, options: {unique: true}}]
-})
--- server/app/collections/VaultCredentials/index.js
+++
@@ -1,8 +0,0 @@
-import {Collection} from '@orion-js/app'
-import VaultCredential from 'app/models/VaultCredential'
-
-export default new Collection({
-  name: 'vault_credentials',
-  model: VaultCredential,
-  indexes: [{keys: {userId: 1, vaultId: 1}, options: {unique: true}}]
-})
