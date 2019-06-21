2262b2e0b975d187c570c5277715aa1ef7cc4a4a
uner4s <nicolas.p.poblete@gmail.com>
2019-06-21 00:01:50 -0400

    create new userV (user vector) for vault passwords

--- server/app/helpers/keys/generateCipherKeys.js
+++ server/app/helpers/keys/generateCipherKeys.js
@@ -13,11 +13,15 @@ export default async function(masterKey) {
 
   const secret = privateKey.slice(secretKeyInterval, secretEnd)
   const iv = publicKey.slice(ivKeyInterval, ivEnd)
+  const userV = privateKey.slice(0, 9) + publicKey.slice(10, 16)
 
-  if (secret.length !== 32 || iv.length !== 16) throw new Error('Error creating keys')
+  if (secret.length !== 32 || iv.length !== 16 || userV.length !== 15) {
+    throw new Error('Error creating keys')
+  }
 
   return {
     secret,
-    iv
+    iv,
+    userV
   }
 }
