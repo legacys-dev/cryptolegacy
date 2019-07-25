71aee81d6b18849855d6fe91d4c57aa4d37da72e
DiegoMellisG <diego.mellis@usach.cl>
2019-07-25 10:37:56 -0400

    update dictionary for emergencyKit

--- web/src/App/i18n/strings/emergencyKit/es.js
+++ web/src/App/i18n/strings/emergencyKit/es.js
@@ -3,10 +3,5 @@ export default{
     the: 'el',
     emergencyKit: 'Kit de emergencia',
     yourMasterKey: 'Tu llave maestra es:',
-<<<<<<< HEAD
-    rememberSave: 'Recuerda guardar este documento en un lugar seguro. Este documento es de vital importancia \n para que puedas acceder a tu cuenta en la plataforma.'
-}
-=======
     rememberSave: 'Recuerda guardar este documento en un lugar seguro. Este documento es de vital importancia para que puedas acceder a tu cuenta en la plataforma.'
  }
->>>>>>> 4ed7242dd5d302e42b343c44164172508241d5d6
