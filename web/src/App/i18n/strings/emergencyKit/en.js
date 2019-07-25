d93019bb5e1719ef3d17075fe8b1f5cd91e403f0
DiegoMellisG <diego.mellis@usach.cl>
2019-07-25 10:06:19 -0400

    update translations on the emergency kit PDF

---
+++ web/src/App/i18n/strings/emergencyKit/en.js
@@ -0,0 +1,7 @@
+export default{
+    createTo: 'Created for',
+    the: 'on',
+    emergencyKit: 'Emergency Kit',
+    yourMasterKey: 'Your master key is:',
+    rememberSave: 'Remember save this document in a safe place. This document is vital for you \n to access your account on the platform.'
+}
