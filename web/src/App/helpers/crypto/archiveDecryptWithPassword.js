b02777944c91f957aea5ba2301877fb3d5f342b9
eliseo3348 <carloseliseo.fuentes.soto@gmail.com>
2019-07-29 18:17:45 -0400

    update

--- web/src/App/helpers/crypto/archiveDecryptWithPassword.js
+++ web/src/App/helpers/crypto/archiveDecryptWithPassword.js
@@ -13,15 +13,7 @@ export default ({encryptedItem, cipherPassword, archiveIv}) => {
 
   const iv = Buffer.from(archiveIv, 'hex')
 
-<<<<<<< HEAD
   const decipher = crypto.createDecipheriv(algorithm, cipherPassword, iv)
-=======
-<<<<<<< HEAD
-  const decipher = crypto.createDecipheriv(algorithm, cipherPassword, iv)
-=======
-  const decipher = crypto.createDecipheriv(algorithm, cipherPassword, Iv)
->>>>>>> ab9a6ad3dce075e58c1d1f83f61555590c647e1d
->>>>>>> 34456dee2b09f66972850ae89826642e501ef5bb
   const decrypted = decipher.update(encryptedItem)
   const finalBuffer = Buffer.concat([decrypted, decipher.final()])
 
