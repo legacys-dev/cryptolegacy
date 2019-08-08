9afd12c921949e96f52117bd5df02e100fe823f1
MichaelCastillo <michael.castillo.p96@gmail.com>
2019-08-08 13:38:44 -0400

    add generatePdf

--- server/app/services/http/home.js
+++ server/app/services/http/home.js
@@ -1,5 +1,18 @@
 import {route} from '@orion-js/app'
-
+import generatepdf from '../../helpers/pdf/generatePdf'
 route('/', async function({params, query, pathname, request, headers, response, getBody}) {
   return 'hello world'
 })
+
+route('/generatepdf', async function({params, query, pathname, request, headers, response, getBody}) {
+  const pdf = require('html-pdf');
+  console.log("Soy el generador de pdf")
+  pdf.create(generatepdf(), {}).toFile('tmp/rezultati.pdf', (err,res) => {
+    if(err) {
+        return console.log('error');
+    }
+    console.log(res)
+    });
+})
+
+
