d9520c41c4a2b8dbb066c1321ffafc0d080a597d
uner4s <nicolas.p.poblete@gmail.com>
2019-08-15 23:18:16 -0400

    Update components for new vaults types and drive incorporation

---
+++ web/src/App/components/Parts/FileManager/Manager/Upload/Warning/typeMessages.js
@@ -0,0 +1,8 @@
+export default {
+  dev: 'La aplicación se esta ejecutando en modo dev',
+  beta:
+    'La aplicación se está ejecutando en modo BETA. Te recomendamos que mientras la pruebes, ' +
+    'no subas archivos que tienen una importancia real para ti, ya que las actualizaciones que sufra la aplicación ' +
+    'harán que tus archivos sean irrecuperables.',
+  local: 'Local mode'
+}
